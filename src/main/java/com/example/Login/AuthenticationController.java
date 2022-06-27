671a45755bced11220a59c685166cee89884d3cd
Justyna Jachowicz <justyna.jachowicz@microsoft.wsei.edu.pl>
2022-06-27 12:42:57 +0200

    added Get method to return list of all registered users, added db, communication between microservices

--- src/main/java/com/example/Login/AuthenticationController.java
+++ src/main/java/com/example/Login/AuthenticationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Controller
 public class AuthenticationController {
@@ -17,7 +18,7 @@ public class AuthenticationController {
     private UserRepository userRepository;
 
     // register a new user
-    //echo '{"username":"mary123", "password":"password123", "email":"mary123@email.com"}' | curl -X POST -H "Content-Type: application/json" -d @- http://localhost:8089/api/user/register
+    // echo '{"username":"mary123", "password":"password123", "email":"mary123@email.com"}' | curl -X POST -H "Content-Type: application/json" -d @- http://localhost:8089/api/user/register
     @RequestMapping(
             method = RequestMethod.POST,
             value = "/api/user/register",
@@ -28,4 +29,17 @@ public class AuthenticationController {
     public ResponseEntity<UserRegisterEntity> registerUser(@RequestBody UserRegisterEntity userEntity) {
         return ResponseEntity.ok(this.userRepository.save(userEntity));
     }
+
+    // get list of registered users
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/api/users",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Transactional
+    public ResponseEntity<List<UserRegisterEntity>> getGuests() {
+        return ResponseEntity.ok(this.userRepository.findAll());
+    }
+
+
 }
--- src/main/java/com/example/Login/UserRepository.java
+++ src/main/java/com/example/Login/UserRepository.java
@@ -3,7 +3,10 @@ package com.example.Login;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository  extends JpaRepository<UserRegisterEntity, Long> {
 
+    List<UserRegisterEntity> findAll();
 }
