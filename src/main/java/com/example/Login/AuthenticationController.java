ff7df9fb90ac0c62f734826e696a15fc3604acce
Justyna Jachowicz <justyna.jachowicz@microsoft.wsei.edu.pl>
2022-06-27 11:31:00 +0200

    Initial commit, register a new user

---
+++ src/main/java/com/example/Login/AuthenticationController.java
@@ -0,0 +1,31 @@
+package com.example.Login;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.transaction.Transactional;
+
+@Controller
+public class AuthenticationController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // register a new user
+    //echo '{"username":"mary123", "password":"password123", "email":"mary123@email.com"}' | curl -X POST -H "Content-Type: application/json" -d @- http://localhost:8089/api/user/register
+    @RequestMapping(
+            method = RequestMethod.POST,
+            value = "/api/user/register",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Transactional
+    public ResponseEntity<UserRegisterEntity> registerUser(@RequestBody UserRegisterEntity userEntity) {
+        return ResponseEntity.ok(this.userRepository.save(userEntity));
+    }
+}
---
+++ src/main/java/com/example/Login/UserRegisterEntity.java
@@ -0,0 +1,69 @@
+package com.example.Login;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserRegisterEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+
+    public UserRegisterEntity() {
+    }
+
+    public UserRegisterEntity(long id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+
+}
